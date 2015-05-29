8cd183d72c1cc810116b29f99559b94976529de4
aa0na <aa0na@arrl.net>
2015-05-29 08:58:41 -0500

    added zulu time clock thread

--- huskylog.py
+++ huskylog.py
@@ -2,62 +2,70 @@
 
 import sqlite3
 import configparser
+import threading
+import time
+import curses
 from curses import wrapper
 from datetime import datetime, timezone
 
+#clock called by threading to stay running in background
+def update_time(stdscr):
+    dims = stdscr.getmaxyx()
+    while 1:
+        date = datetime.now(timezone.utc).strftime("%Y-%m-%d")
+        hour = datetime.now(timezone.utc).strftime("%H:%M:%S")
+        stdscr.addstr(dims[0]-1, int(dims[1]/2)-17, 'Zulu Date Time: '+date+" "+hour)
+        stdscr.refresh()
+        time.sleep(1)
+
 #define new callsign
 def oper():
-  oper = input('operator callsign: ')
+    oper = input('operator callsign: ')
 
 #define location for use with lotw
 #new qth
 #adds new lotw location to database also
 def nqth():
-  mqth = input('operator location: ')
+    mqth = input('operator location: ')
 
 def powr():
-  powr = input('operator tx power: ')
+    powr = input('operator tx power: ')
 
 def setband():
-  band = input('operator tx band: ')
-
-#read huskylog.conf for settings
-# if huskylog.conf is empty, prompt for fields and create it
-config = configparser.ConfigParser()
-config.read('huskylog.conf')
+    band = input('operator tx band: ')
 
 def getdefault(value):
-  confvalue = value
-  try:
-    value = config['DEFAULT'][confvalue]
-  except:
-    print( confvalue + '... not found in huskylog.conf')
-    value = input('input value: ')
-    config['DEFAULT'][confvalue] = value
-  return value
+    confvalue = value
+    try:
+        value = config['DEFAULT'][confvalue]
+    except:
+        print( confvalue + '... not found in huskylog.conf')
+        value = input('input value: ')
+        config['DEFAULT'][confvalue] = value
+    return value
 
 def writeconf():
-  with open('huskylog.conf', 'w') as configfile:
-    config.write(configfile)
+    with open('huskylog.conf', 'w') as configfile:
+      config.write(configfile)
 
 def lotw():
-  print('lotw') #edits lotw table
+    print('lotw') #edits lotw table
 
 def importfile():
-  print('import')
+    print('import')
 
 def exportfile():
-  print('export')
+    print('export')
 
 #open database by callsign stored in huskylog.conf
 #create table if it does not exist
 def connect2db():
-  conn = sqlite3.connect(oper +'.db')
-  db = conn.cursor()
-  db.execute('''CREATE TABLE IF NOT EXISTS
+    conn = sqlite3.connect(oper +'.db')
+    db = conn.cursor()
+    db.execute('''CREATE TABLE IF NOT EXISTS
     logbook(
     date TEXT,
-    time TEXT,
+    hour TEXT,
     band TEXT,
     mode TEXT,
     call TEXT,
@@ -71,66 +79,65 @@ def connect2db():
     eqsl TEXT
     )
     ''')
-  db.close()
+    db.close()
 
 #display last 10 entries in database base
 def tail():
-  print('tail')
-  b = sqlite3.connect('data/mydb')
-  db.row_factory = sqlite3.Row
-  cursor = db.cursor()
-  cursor.execute('''SELECT date, time, band, mode, call, srst, rrst, note FROM logbook''')
-  for row in cursor:
-    # row['name'] returns the name column in the query, row['email'] returns email column.
-    print('{0} : {1}, {2}'.format(row['date'], row['time'], row['band'], row['mode'], row['call'], row['srst'], row['rrst'], row['note']))
-  db.close()
+    print('tail')
+    b = sqlite3.connect('data/mydb')
+    db.row_factory = sqlite3.Row
+    cursor = db.cursor()
+    cursor.execute('''SELECT date, hour, band, mode, call, srst, rrst, note FROM logbook''')
+    for row in cursor:
+        # row['name'] returns the name column in the query, row['email'] returns email column.
+        print('{0} : {1}, {2}'.format(row['date'], row['hour'], row['band'], row['mode'], row['call'], row['srst'], row['rrst'], row['note']))
+    db.close()
 
 #read from database
 def readdb():
-  print('readdb')
+    print('readdb')
+
 #insert qso in database
 def insert(n):
-  db.execute('''INSERT INTO
-    logbook VALUES(
-    date TEXT,
-    time TEXT,
-    band TEXT,
-    mode TEXT,
-    call TEXT,
-    srst TEXT,
-    rrst TEXT,
-    note TEXT,
-    mqth TEXT,
-    powr TEXT,
-    lotw TEXT,
-    eqsl TEXT
-    )
-    ''')
+    db.execute('''INSERT INTO
+        logbook VALUES(
+        date TEXT,
+        hour TEXT,
+        band TEXT,
+        mode TEXT,
+        call TEXT,
+        srst TEXT,
+        rrst TEXT,
+        note TEXT,
+        mqth TEXT,
+        powr TEXT,
+        lotw TEXT,
+        eqsl TEXT
+        )
+        ''')
 
 def display(stdscr):
-  dims = stdscr.getmaxyx()
-  stdscr.addstr(0, 0, 'Operator: ' + oper)
-  stdscr.addstr(1, 0, 'Location: ' + mqth)
-  stdscr.addstr(0, int(dims[1]/2)-4, 'Huskylog')
-  stdscr.addstr(0, dims[1]-15, 'Power: ' + powr)
-  stdscr.addstr(1, dims[1]-15, 'Band : ' + band)
-  stdscr.addstr(2, dims[1]-15, 'Mode : ' + mode)
-  stdscr.addstr(3, 0, 'Date')
-  stdscr.addstr(3, 9, 'Time')
-  stdscr.addstr(3, 17, 'Band')
-  stdscr.addstr(3, 23, 'Mode')
-  stdscr.addstr(3, 29, 'Callsign')
-  stdscr.addstr(3, 40, 'SRST')
-  stdscr.addstr(3, 45, 'RRST')
-  stdscr.addstr(3, 51, 'Comments')
-  stdscr.addstr(4, 0, '-' * dims[1])
-  stdscr.addstr(dims[0]-2, 0, '-' * dims[1])
-  stdscr.addstr(dims[0]-1, 0, '*Enter New Contact*')
-  stdscr.addstr(dims[0]-1, int(dims[1]/2)-17, 'Zulu Date Time: '+date+" "+time)
-
-  stdscr.addstr(dims[0]-1, dims[1]-36, 'Type .help for list of all commands')
-  stdscr.move(dims[0]-3, 0)
-  stdscr.refresh()
+    dims = stdscr.getmaxyx()
+    stdscr.addstr(0, 0, 'Operator: ' + oper)
+    stdscr.addstr(1, 0, 'Location: ' + mqth)
+    stdscr.addstr(0, int(dims[1]/2)-4, 'Huskylog')
+    stdscr.addstr(0, dims[1]-15, 'Power: ' + powr)
+    stdscr.addstr(1, dims[1]-15, 'Band : ' + band)
+    stdscr.addstr(2, dims[1]-15, 'Mode : ' + mode)
+    stdscr.addstr(3, 0, 'Date')
+    stdscr.addstr(3, 9, 'Time')
+    stdscr.addstr(3, 17, 'Band')
+    stdscr.addstr(3, 23, 'Mode')
+    stdscr.addstr(3, 29, 'Callsign')
+    stdscr.addstr(3, 40, 'SRST')
+    stdscr.addstr(3, 45, 'RRST')
+    stdscr.addstr(3, 51, 'Comments')
+    stdscr.addstr(4, 0, '-' * dims[1])
+    stdscr.addstr(dims[0]-2, 0, '-' * dims[1])
+    stdscr.addstr(dims[0]-1, 0, '*Enter New Contact*')
+    stdscr.addstr(dims[0]-1, dims[1]-36, 'Type .help for list of all commands')
+    stdscr.move(dims[0]-3, 0)
+    stdscr.refresh()
 
 def getinput(stdscr):
     kbuf = stdscr.getch()
@@ -139,10 +146,16 @@ def getinput(stdscr):
 #if more than one, ask which one to edit and confirm
 #option to delete entry in database and confirm
 def edit(n):
-  print('edit')
+    print('edit')
+
+#read huskylog.conf for settings
+# if huskylog.conf is empty, prompt for fields and create it
+config = configparser.ConfigParser()
+config.read('huskylog.conf')
+
 
 date = datetime.now(timezone.utc).strftime("%Y-%m-%d")
-time = datetime.now(timezone.utc).strftime("%H:%M:%S")
+hour = datetime.now(timezone.utc).strftime("%H:%M:%S")
 oper = getdefault('oper')
 mqth = getdefault('mqth')
 powr = getdefault('powr')
@@ -153,14 +166,18 @@ kbuf = ""
 connect2db()
 
 def main (stdscr):
-  display(stdscr)
-  dims = stdscr.getmaxyx()
-  
-  while True:
-    kbuf = stdscr.getkey()
-    stdscr.addstr(dims[0]-3, 0, kbuf)
-    if kbuf == "q": break
-  
+    display(stdscr)
+    clock = threading.Thread(target=update_time, args=(stdscr,))
+    clock.daemon = True
+    clock.start()
+    dims = stdscr.getmaxyx()
+    
+    while 1:
+        kbuf = stdscr.getkey()
+        stdscr.addstr(dims[0]-3, 0, kbuf)
+        if kbuf == "q": break
+        #stdscr.refresh()
+        
 wrapper(main)
 
 writeconf()
